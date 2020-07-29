6f15e3d7bb04e0e17bae494e58a2f194210a59e2
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-29 19:46:48 +0000

    new file with connect function and being exported

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+const connect = function(){
+  const conn = net.createConnection({
+    host: '127.0.0.1',
+    port: 50541
+  })
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data)=>{
+    console.log(data)
+  })
+
+  return conn;
+}
+
+module.exports = {connect};
