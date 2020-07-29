b193c8b9ad07b79bfc6fa6f94a9fcf6bf6c4ad44
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-29 20:09:05 +0000

    connect event handler added so name and logging message are printed when successfully connected

--- client.js
+++ client.js
@@ -1,16 +1,20 @@
 const net = require('net');
-const connect = function(){
+const connect = function () {
   const conn = net.createConnection({
     host: '127.0.0.1',
     port: 50541
   })
   conn.setEncoding('utf8');
 
-  conn.on('data', (data)=>{
+  conn.on('data', (data) => {
     console.log(data)
+  });
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SYH");
   })
 
   return conn;
 }
 
-module.exports = {connect};
+module.exports = { connect };
