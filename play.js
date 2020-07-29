c3514a4ca2aa768368b1b975e3de7ccebe44c29e
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-29 19:45:17 +0000

    refactored by requiring the connect function from client.js

--- play.js
+++ play.js
@@ -1,18 +1,6 @@
-const net = require('net');
 
-const connect = function(){
-  const conn = net.createConnection({
-    host: '127.0.0.1',
-    port: 50541
-  })
-  conn.setEncoding('utf8');
-
-  conn.on('data', (data)=>{
-    console.log(data)
-  })
-
-  return conn;
-}
+const { connect } = require('./client');
 
 console.log('connecting......');
+
 connect();
