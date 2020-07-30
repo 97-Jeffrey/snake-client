fb5fe0074132c277caccd4a5bf8ab714f1ce9b98
Jeffrey Shao <shaoyuhao970909@gmail.com>
2020-07-30 00:31:06 +0000

    some changes to it

--- input.js
+++ input.js
@@ -13,33 +13,33 @@ const setupInput = function (conn) {
       process.exit();
     }
     if (key === 'w') {
-     
+    
       connection.write('Move: up');
     }
     if (key === 's') {
-     
+
       connection.write('Move: down');
     }
     if (key === 'a') {
-     
+
       connection.write('Move: left');
     }
     if (key === 'd') {
-     
+
       connection.write('Move: right');
     }
-    if(key === 'm'){
-    connection.write('Say: good job!!');
+    if (key === 'm') {
+      connection.write('Say: good job!!');
     }
-    if(key === 'k'){
-    connection.write('Say: haha, I \'m the boss');
+    if (key === 'k') {
+      connection.write('Say: haha, I \'m the boss');
     }
-    if(key === 'n'){
-    connection.write('Say: ')
+    if (key === 'n') {
+      connection.write('Say: ')
     }
 
   })
-  
+
   return stdin;
 }
 
